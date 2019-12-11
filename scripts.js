524f1f4acd71dc522aa6f9a08c83555b75fc4fb4
JetskevdWouden <jetske.vanderwouden@wetransfer.com>
2019-12-11 11:36:31 +0100

    import object

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-import * as THREE from "./node_modules/three/build/three.module";
-import { OrbitControls } from "./node_modules/three/examples/jsm/controls/OrbitControls";
-import { GLTFLoader } from "./node_modules/three/examples/jsm/loaders/GLTFLoader";
+import * as THREE from "./node_modules/three/build/three.module.js";
+import { OrbitControls } from "./node_modules/three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "./node_modules/three/examples/jsm/loaders/GLTFLoader.js";
 
 function main() {
   const canvas = document.querySelector("#c");
